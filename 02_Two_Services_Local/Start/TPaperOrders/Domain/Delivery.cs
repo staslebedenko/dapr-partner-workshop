e212453415219c7f89ddf1301e6e2f429c42ec53
Stas Lebedenko <>
2021-09-21 16:36:34 +0300

    Step 2 start

---
+++ 02_Two_Services_Local/Start/TPaperOrders/Domain/Delivery.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TPaperOrders
+{
+    public class Delivery
+    {
+        public Delivery()
+        {
+            this.Orders = new HashSet<EdiOrder>();
+        }
+
+        public int Id { get; set; }
+
+        public decimal Number { get; set; }
+
+        public int ClientId { get; set; }
+
+        public int EdiOrderId { get; set; }
+
+        public int ProductCode { get; set; }
+
+        public int ProductId { get; set; }
+
+        public string Notes { get; set; }
+
+        public EdiOrder EdiOrder { get; set; }
+
+        public ICollection<EdiOrder> Orders { get; }
+    }
+}
