549c2afec23f135b91d798c32a94027523fb5fc6
Stas Lebedenko <>
2021-09-21 16:55:11 +0300

    Step 2 end

---
+++ 03_Two_Services_Cloud/End/TPaperOrders/Data/PaperDbContext.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace TPaperOrders
+{
+    public class PaperDbContext : DbContext
+    {
+        public PaperDbContext(DbContextOptions<PaperDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<EdiOrder> EdiOrder { get; set; }
+        
+        public static void ExecuteMigrations(string connectionString)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<PaperDbContext>();
+            optionsBuilder.UseSqlServer(connectionString);
+
+            using var context = new PaperDbContext(optionsBuilder.Options);
+            try
+            {
+                context.Database.Migrate();
+            }
+            catch (Exception e)
+            {
+                throw new Exception($"Error when migrating database: {e.Message}");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasDefaultSchema("dbo");
+            PaperContextConfiguration.Configure(modelBuilder);
+            base.OnModelCreating(modelBuilder);
+        }
+
+        /*
+         * dotnet ef migrations add SecondMigration --context PaperDbContext
+         * dotnet ef database update
+         * dotnet ef migrations remove --context PaperDbContext
+        */
+    }
+}
---
+++ 03_Two_Services_Cloud/End/TPaperOrders/Domain/EdiOrder.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace TPaperOrders
+{
+    public class EdiOrder
+    {
+        public int Id { get; set; }
+
+        public int ClientId { get; set; }
+
+        public decimal Quantity { get; set; }
+
+        public int ProductCode { get; set; }
+
+        public string Notes { get; set; }
+
+        public int? DeliveryId { get; set; }
+
+        public bool IsValid()
+        {
+            return Quantity > 0;
+        }
+    }
+}
